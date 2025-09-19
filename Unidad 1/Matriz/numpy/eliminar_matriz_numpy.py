ab18570feeb1ee56f36aa9cd58421ebfbba4e4a3
Ashley-Dayanne <ux24ii240@ux.edu.mx>
2025-09-19 09:44:22 -0500

    Ejercicios clase 19/09/25

---
+++ Unidad 1/Matriz/numpy/eliminar_matriz_numpy.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# 1. Crear un conjunto de datos (Matriz de 3x4)
+# La segunda columna (Indice 1) es la que se eliminará
+datos = np.array([
+    [10,20,30,40],
+    [15,20,35,45],
+    [25,20,45,55]
+])
+
+print("---- conjunto de datos original ----")
+print(datos)
+
+# 2. Eliminar la columna de datos irrelevantes
+# np.delete() se utiliza para eliminar elementos o filas/columnas
+# axis=1 indica que se debe eliminar una columna
+# El segundo argumento es el índice de la columna a eliminar (en este caso, 1)
+datos_limpios = np.delete(datos, 2, axis=0)
+
+print("\n---- Conjunto de datos limpio ----")
+print(datos_limpios)
+
+# Valores para axis
+# axis=1 -> columnas
+# axis=0 -> filas
+# axis=None -> todo el array
+datos_limpios2 = np.delete(datos_limpios, 0, axis=1)
+
+print("\n---- Conjunto de datos limpios procesados ----")
+print(datos_limpios2)
